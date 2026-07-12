e250adbc0b8a71f7b6943953a8228f95e73e162d
Arya Nugraha <a40268375@gmail.com>
2026-07-12 10:30:56 +0700

    menghapus app di dalamn frontennd

--- frontend/app/Models/Business.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class Business extends Model {
-    protected $fillable = ['user_id', 'category_id', 'title', 'description', 'address', 'phone', 'email', 'operating_hours', 'website', 'image', 'is_premium', 'status', 'referred_by'];
-}
